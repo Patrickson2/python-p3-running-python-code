89a436d244c09bb81d4bebcf311e8904b454c8c7
Patrickson2 <thairupatrickson@gmail.com>
2025-11-11 12:33:34 +0300

    addes an app.py file

---
+++ lib/app.py
@@ -0,0 +1,11 @@
+# this is a comment used in python just like pressing on the ctr + / buttons in vannila javascript
+# to run this application head to terminal and call the location of the file you want to run first 
+# eg:for this file i will call python3 lib/app.py
+# you can choose how you want the python() to be outputed at the end while using the END parameter 
+# eg print("welcom to my world!", end="Mungai")
+
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
+
+print("Hello World! Pass this test, please.")
